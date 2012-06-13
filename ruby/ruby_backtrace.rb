ebe2e5b3fde4c2ff046a9370b9006581dea0c4d8
Le Huy <lehuy20@gmail.com>
2012-06-13 07:14:55 +0200

    add support for ruby intepreter without Thread#backtrace and python

--- ruby/ruby_backtrace.rb
+++ ruby/ruby_backtrace.rb
@@ -1,12 +1,17 @@
 require 'pp'
 
 def backtrace_for_all_threads(signame)
-    File.open("./ruby_backtrace_#{Process.pid}.txt","a") do |f|
+    File.open("/tmp/ruby_backtrace_#{Process.pid}.txt","a") do |f|
       f.puts "--- got signal #{signame}, dump backtrace for all threads at #{Time.now}"
-      Thread.list.each do |t|
-         f.puts t.inspect
-         PP.pp(t.backtrace.delete_if {|frame| frame =~ /^#{File.expand_path(__FILE__)}/},
+      if Thread.current.respond_to?(:bactrace)
+        Thread.list.each do |t|
+          f.puts t.inspect
+          PP.pp(t.backtrace.delete_if {|frame| frame =~ /^#{File.expand_path(__FILE__)}/},
                f) # remove frames resulting from calling this method
+        end
+      else
+        f.puts Thread.current.inspect
+        PP.pp(caller)
       end
   end
 end
